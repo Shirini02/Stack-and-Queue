91688aa55bdf208038d5103278111c032a5e4b11
Shirini02 <shirini.gone@xcubelabs.com>
2021-09-02 23:13:29 -0700

    Done with the task of stack and queue

--- Assets/QueuesExample.cs
+++ Assets/QueuesExample.cs
@@ -7,26 +7,39 @@ public class QueuesExample : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI queueDebug;
+    int test = 0;
+    public GameObject[] prefabs;
     // Start is called before the first frame update
     void Start()
     {
         Queue queue = new Queue();
-        queue.Enqueue("x");
-        queue.Enqueue("y");
-        queue.Enqueue("z");
-        queue.Enqueue("a");
-        queue.Enqueue("b");
+        /* queue.Enqueue("x");
+         queue.Enqueue("y");
+         queue.Enqueue("z");
+         queue.Enqueue("a");
+         queue.Enqueue("b");*/
+        queue.Enqueue(prefabs[0]);
+        queue.Enqueue(prefabs[1]);
+        queue.Enqueue(prefabs[2]);
+        queue.Enqueue(prefabs[3]);
         showinformationInQueue(queue);
         queue.Dequeue();
         showinformationInQueue(queue);
+        queue.Dequeue();
+        showinformationInQueue(queue);
+
+
 
     }
     private void showinformationInQueue(Queue queue)
     {
         queueDebug.text = ".............\n";
-        foreach (var queueitem in queue)
+        foreach (GameObject queueitem in queue)
         {
             queueDebug.text += $"{queueitem}\n";
+            Instantiate(queueitem, transform.position + new Vector3(test, 0, 0), Quaternion.identity);
+            test = test + 2;
+            print($"{queueitem}\n");
         }
 
        
--- Assets/StackExample.cs
+++ Assets/StackExample.cs
@@ -7,18 +7,25 @@ public class StackExample : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI stackDebug;
+    int test = 0;
+    public GameObject[] prefabs;
     // Start is called before the first frame update
     void Start()
     {
         Stack stack = new Stack();
         //Prep our data
-        int firstPlayerScore = 12;
-        int secondplayerscore = 10;
-        int thirdplayerscore = 100;
-        //push a new player score in to the stack
-        stack.Push(firstPlayerScore);
-        stack.Push(secondplayerscore);
-        stack.Push(thirdplayerscore);
+        /* int firstPlayerScore = 12;
+         int secondplayerscore = 10;
+         int thirdplayerscore = 100;
+         //push a new player score in to the stack
+         stack.Push(firstPlayerScore);
+         stack.Push(secondplayerscore);
+         stack.Push(thirdplayerscore);*/
+        stack.Push(prefabs[0]);
+        stack.Push(prefabs[1]);
+        stack.Push(prefabs[2]);
+        stack.Push(prefabs[3]);
+
 
         //show information in stack
         showinformationInstack(stack);
@@ -33,11 +40,14 @@ public class StackExample : MonoBehaviour
     void showinformationInstack(Stack stack)
     {
         stackDebug.text += "......................\n";
-        foreach(var stackitem in stack)
+        foreach(GameObject stackitem in stack)
         {
             Debug.Log(stack.Count);
            //stackDebug.text += "\n";
             stackDebug.text += $"{stackitem}\n";
+            Instantiate(stackitem, transform.position + new Vector3(test, 0, 0), Quaternion.identity);
+            test = test + 2;
+            print($"{stackitem}\n");
             //stackDebug.text = "\n";
         }
     }
